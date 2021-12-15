3094dcf4c4eea533a0b9863d6401819376358261
Raider144 <lawrence.isaac970@gmail.com>
2021-12-14 19:34:35 -0700

    getting ready

--- frontend/App.js
+++ frontend/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text>leggdy</Text>
+			<Text>lboomdy</Text>
 			<StatusBar style="auto" />
 		</View>
 	);
