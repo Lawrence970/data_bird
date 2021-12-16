7e897f90e10a27ce35605132093bc1234ccd1109
Robby Bennett <robby@scitools.com>
2021-12-15 19:37:02 -0700

    tabs, baby

--- backend/persist.js
+++ backend/persist.js
@@ -4,26 +4,26 @@ const mongoose = require("mongoose");
 const db = mongoose.connection;
 
 function connect(callback) {
-  // connection to mongo DB atlas
-  
-  mongoose
-    .connect(connectionString, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-    .catch(err => {
-      console.log("there was an error connecting to mongo: ", err);
-    });
+	// connection to mongo DB atlas
+	
+	mongoose
+		.connect(connectionString, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		})
+		.catch(err => {
+			console.log("there was an error connecting to mongo: ", err);
+		});
 }
 
 // on this db connection, once open, run the 'callback'
 // a wrapper method
 function onConnect(callback) {
-  db.once("open", callback);
+	db.once("open", callback);
 }
 
 // export as an object
 module.exports = {
-  connect: connect,
-  onConnect: onConnect
+	connect: connect,
+	onConnect: onConnect
 };
